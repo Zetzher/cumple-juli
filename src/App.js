10e9dc65c395045054c898b1584c9c00fa0fe4e7
zetzher <j.abasolo.agenjo@gmail.com>
2022-11-02 22:18:59 +0100

    Almost finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const arr = [
+    { name: "Armando" },
+    { name: "Javi" },
+    { name: "Alejandro" },
+    { name: "Joan" },
+    { name: "Cristobal" },
+    { name: "Julian" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="first-section">
+      {arr.map((data) => (
+        <div className="container">
+          <div className="card">
+            <div className="image">
+              <img src="https://i.pinimg.com/originals/a4/7b/a5/a47ba59b4a353e0928ef0551ca44f980.jpg" />
+            </div>
+            <div className="content">
+              <h3>This is content</h3>
+              <p>
+                DIn publishing and graphic design, Lorem ipsum is a placeholder
+                text commonly used to demonstrate the visual form of a document
+                or a typeface without relying on meaningful content.
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
   );
-}
+};
 
 export default App;

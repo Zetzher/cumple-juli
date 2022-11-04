0ccb8b73822ebf83a8a773f3983a08fcc403c60e
zetzher <j.abasolo.agenjo@gmail.com>
2022-11-04 16:48:53 +0100

    Text almost finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-import logo from "./logo.svg";
+import armandoPx from "./assets/armandoDef.gif";
+import cristobalPx from "./assets/cristobalDef.gif";
+import julianPx from "./assets/julianDef.gif";
+import alejandroPx from "./assets/alejandroDef.gif";
+import joanPx from "./assets/joanDef.gif";
+import javiPx from "./assets/javiDef.gif";
+import armandoFoto from "./assets/foto-armando.png";
+import cristobalFoto from "./assets/foto-cristobal.png";
+import javiFoto from "./assets/foto-javi.png";
+import alejandroFoto from "./assets/foto-alejandro.png";
+import julianFoto from "./assets/foto-julian.png";
+import joanFoto from "./assets/foto-joan.png";
 import "./App.css";
 
 const App = () => {
@@ -11,25 +22,114 @@ const App = () => {
     { name: "Julian" },
   ];
 
+  const dynamicPhoto = ({ key }) => {
+    switch (key) {
+      case 0:
+        return armandoFoto;
+      case 1:
+        return javiFoto;
+      case 2:
+        return cristobalFoto;
+      case 3:
+        return joanFoto;
+      case 4:
+        return alejandroFoto;
+      case 5:
+        return julianFoto;
+    }
+  };
+
+  const dynamicPx = ({ key }) => {
+    switch (key) {
+      case 0:
+        return armandoPx;
+      case 1:
+        return javiPx;
+      case 2:
+        return cristobalPx;
+      case 3:
+        return joanPx;
+      case 4:
+        return alejandroPx;
+      case 5:
+        return julianPx;
+    }
+  };
+
+  const dymanicResult = ({ type, key }) => {
+    switch (key) {
+      case 0:
+        if (type === "picture") {
+          return armandoFoto;
+        } else if (type === "text") {
+          return "Chupa y mama que se derrama";
+        } else {
+          return armandoPx;
+        }
+      case 1:
+        if (type === "picture") {
+          return javiFoto;
+        } else if (type === "text") {
+          return "Nunca pensé que hacer la ventosa contigo nos uniera tanto";
+        } else {
+          return javiPx;
+        }
+      case 2:
+        if (type === "picture") {
+          return cristobalFoto;
+        } else if (type === "text") {
+          return "Eres la guarra mas bonica q conozco";
+        } else {
+          return cristobalPx;
+        }
+      case 3:
+        if (type === "picture") {
+          return joanFoto;
+        } else if (type === "text") {
+          return "Falta texto";
+        } else {
+          return joanPx;
+        }
+      case 4:
+        if (type === "picture") {
+          return alejandroFoto;
+        } else if (type === "text") {
+          return "Felicidades maricón, espero que los años te hagan más sabia pero no menos guarra";
+        } else {
+          return alejandroPx;
+        }
+      case 5:
+        if (type === "picture") {
+          return julianFoto;
+        } else if (type === "text") {
+          return "Que ni mil fiestas, ni mil rabos nos separen, guarrilla mía";
+        } else {
+          return julianPx;
+        }
+    }
+  };
+
   return (
     <section className="first-section">
-      {arr.map((data) => (
-        <div className="container">
-          <div className="card">
-            <div className="image">
-              <img src="https://i.pinimg.com/originals/a4/7b/a5/a47ba59b4a353e0928ef0551ca44f980.jpg" />
-            </div>
-            <div className="content">
-              <h3>This is content</h3>
-              <p>
-                DIn publishing and graphic design, Lorem ipsum is a placeholder
-                text commonly used to demonstrate the visual form of a document
-                or a typeface without relying on meaningful content.
-              </p>
+      {arr.map((data, key) => {
+        return (
+          <div className="container">
+            <div className="card">
+              <div className="image">
+                <img src={dynamicPhoto({ key })} />
+              </div>
+              <div className="content">
+                <h3>{dymanicResult({ type: "text", key })}</h3>
+                <img
+                  src={dynamicPx({ key })}
+                  style={{ width: 50, marginLeft: 10 }}
+                />
+                <p></p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </section>
   );
 };
